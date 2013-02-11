58ff0d66d5aaacdf1382813376efb88003645f74
Christoffer Johnsson <stoffe1992@hotmail.com>
2013-02-11 13:45:47 +0100

    Initial commit, added image content
    Signed-off-by: Christoffer Johnsson <stoffe1992@hotmail.com>

---
+++ src/RaceGame/RaceGame/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace RaceGame
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        private static RaceGame game;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            game = new RaceGame();
+            game.Run();
+        }
+    }
+}
