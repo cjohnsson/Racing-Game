7ed113b22f0ff5db78b567c7fbef97837c153cb0
Christoffer Johnsson <stoffe1992@hotmail.com>
2013-02-11 15:52:40 +0100

    Added tests for car
    Signed-off-by: Christoffer Johnsson <stoffe1992@hotmail.com>

---
+++ src/RaceGame/RaceGame/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RaceGame
+{
+    public class Car
+    {
+        public Car()
+        {
+            Speed = 0;
+        }
+
+        private float _speed;
+        public float Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public void Accelerate()
+        {
+            Speed += 0.1f;
+        }
+    }
+}
