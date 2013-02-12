bf72239a4536ca09598f4492565b2f428a607733
Christoffer Johnsson <stoffe1992@hotmail.com>
2013-02-12 11:03:57 +0100

    Added Control.cs
    Signed-off-by: Christoffer Johnsson <stoffe1992@hotmail.com>

--- src/RaceGame/RaceGame/Control.cs
+++ src/RaceGame/RaceGame/Control.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace RaceGame_Tests
+namespace RaceGame
 {
-    class Controls_Test
+    class Control
     {
     }
 }
