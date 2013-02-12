98f8052f792d74e6fd7327efbcb1fa933db9f87e
Christoffer Johnsson <stoffe1992@hotmail.com>
2013-02-12 20:52:29 +0100

    Changed the starting position for the Car.cs
    Signed-off-by: Christoffer Johnsson <stoffe1992@hotmail.com>

--- src/RaceGame/RaceGame/RaceGame/RaceGame.cs
+++ src/RaceGame/RaceGame/RaceGame/RaceGame.cs
@@ -74,7 +74,7 @@ namespace RaceGame
             bitmap = new Bitmap(stream);
             
             List<Player> players = new List<Player>();
-            players.Add(new Player(new Control(Keys.W, Keys.S, Keys.A, Keys.D), Content.Load<Texture2D>("car-emil"), new Vector2(50, 50), bitmap));
+            players.Add(new Player(new Control(Keys.W, Keys.S, Keys.A, Keys.D), Content.Load<Texture2D>("car-emil"), new Vector2(80, 270), bitmap));
             world = new World(new Map(Content.Load<Texture2D>("map"),Content.Load<Texture2D>("mapforeground")), players );
         }
 
