8d85bab5217b53f6c9f51a3db9d0f971bb7a481e
mofo <fredrik@tjornfelt.com>
2013-02-12 09:50:58 +0100

    car and player classes added by fidde

--- src/RaceGame/RaceGame/Car.cs
+++ src/RaceGame/RaceGame/Car.cs
@@ -2,26 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
 
 namespace RaceGame
 {
-    public class Car
+    class Car
     {
-        public Car()
+        Texture2D image;
+        private Rectangle Position;
+
+        public Rectangle position
         {
-            Speed = 0;
+            get { return Position; }
         }
 
-        private float _speed;
-        public float Speed
+        bool checkPoint;
+        float speed;
+        float rotation;
+        Vector2 origin;
+
+        float x;
+        float y;
+        int height;
+        int width;
+
+        const float ACCELERATION = 0.1f;
+        const float DECELERATION = 0.1f;
+        const float BREAK_DECELERATION = 0.1f;
+
+        void Accelerate()
         {
-            get { return _speed; }
-            set { _speed = value; }
+
         }
 
-        public void Accelerate()
+        void Break()
         {
-            Speed += 0.1f;
+
         }
+
+        void TurnLeft()
+        {
+            if(Keyboard.GetState().IsKeyDown(Keys.Left)
+                
+
+        }
+
+        void TurnRight()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        //TerrainType GetTerrain()
+        //{ 
+        //    return null;
+        //}
+
+        void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(image, position, Color.White);
+        }
+
+        Vector2 GetOrigin()
+        {
+            return new Vector2(1, 2);
+        }
+
     }
 }
---
+++ src/RaceGame/RaceGame/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace RaceGame
+{
+    class Player
+    {
+        Car car;
+        int lap;
+        Control control;
+        string name;
+
+        public Player()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //spriteBatch.Draw()
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
