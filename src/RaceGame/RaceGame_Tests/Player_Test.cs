fd87ce8d3b1f5e1e5f75cce65309a26f0186915a
Mattias Noren <mattiasnoren89@gmail.com>
2013-02-12 11:46:45 +0100

    lite prylar (ps stoffe luktar fisk idag igen)

---
+++ src/RaceGame/RaceGame_Tests/Player_Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RaceGame;
+
+namespace RaceGame_Tests {
+
+    [TestFixture]
+    class Player_Test {
+
+        [Test]
+        public void SetName_InsertName_CheckIfStringIsEmpty()
+        {
+            Control control = new Control();
+            Player player = new Player(control);
+
+            player.Name = "";
+            Assert.That(player.Name, Is.StringContaining(""),"Please type a valid name!");
+        }
+    }
+}
