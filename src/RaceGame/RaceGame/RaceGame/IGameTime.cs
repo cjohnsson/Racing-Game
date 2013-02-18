74fa17d372e61469016e5e59e34c2946bb3f6499
PhilipAdlergard <philip.adlergard@hotmail.com>
2013-02-18 13:07:55 +0100

    gametime class created

---
+++ src/RaceGame/RaceGame/RaceGame/IGameTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RaceGame
+{
+    interface IGameTime
+    {
+        TimeSpan GetElapsedTime();
+        void Pause();
+        void Resume();
+        void Reset();
+    }
+}
